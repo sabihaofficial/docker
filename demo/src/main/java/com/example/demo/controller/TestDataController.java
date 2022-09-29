b031c31012832849dfdcc857d4a7317827bd0a4d
sabihaofficial <sabiha.chougle5@gmail.com>
2022-09-29 20:35:54 +0530

    changing path

--- demo/src/main/java/com/example/demo/controller/TestDataController.java
+++ demo/src/main/java/com/example/demo/controller/TestDataController.java
@@ -4,7 +4,6 @@ import com.example.demo.domain.TestData;
 import com.example.demo.service.TestDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
