bad433169fbd54b9c0ce08b4e4de9c6675dbc9ff
sabihaofficial <sabiha.chougle5@gmail.com>
2022-09-30 20:31:09 +0530

    updated application.properties

--- demo/src/main/java/com/example/demo/data/TestDataRepository.java
+++ demo/src/main/java/com/example/demo/data/TestDataRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.data;
 
 import com.example.demo.domain.TestData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TestDataRepository extends JpaRepository<TestData, Long>
 {
 
--- demo/src/main/java/com/example/demo/service/TestDataService.java
+++ demo/src/main/java/com/example/demo/service/TestDataService.java
@@ -3,11 +3,12 @@ package com.example.demo.service;
 import com.example.demo.data.TestDataRepository;
 import com.example.demo.domain.TestData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Service
 public class TestDataService {
 
     @Autowired
